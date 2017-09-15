19f21aac0ecdb7968ea9bb0af31de5e72924309f
hnzxb <93220077@qq.com>
2017-09-15 23:00:01 +0800

    首次提交

---
+++ src/foo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper" @click="update">
+    <image :src="logoUrl" class="logo"></image>
+    <text class="title">Hello {{target}}</text>
+  </div>
+</template>
+
+<style>
+  .wrapper { align-items: center; margin-top: 120px; }
+  .title { font-size: 48px; }
+  .logo { width: 360px; height: 82px; }
+</style>
+
+<script>
+  //import logo from './assets/img/logo.png';
+  export default {
+    data: {
+      logoUrl: "./assets/img/logo.png",
+      target: 'World'
+    },
+    methods: {
+      update: function (e) {
+        this.target = 'iWeex'
+        console.log('target:', this.target)
+      }
+    }
+  }
+</script>
